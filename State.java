2aa70f7a5c20cee1390439f4444cd98a66f94b4a
CalendiniAbelli <82089771+CalendiniAbelli@users.noreply.github.com>
2021-04-18 18:48:21 +0200

    Update State.java

--- State.java
+++ State.java
@@ -1,9 +1,14 @@
 package pandemie;
 import javafx.scene.paint.Color;
 
+/**
+ * @author Laetitia Abelli
+ * @version 2021.04.18
+ */
+
 public enum State {
 
-    SUSCEPTIBLE(Color.YELLOW), INFECTED(Color.RED), RECOVERED(Color.GREEN), DEAD(Color.BLACK), VACCINATED(Color.PINK);
+    SUSCEPTIBLE(Color.YELLOW), INFECTED(Color.RED), RECOVERED(Color.BLUE), DEAD(Color.BLACK), VACCINATED(Color.PINK);
 
     protected Color color;
 
@@ -15,16 +20,28 @@ public enum State {
         this.color= color;
     }
 
+    /**
+     *
+     * @return color (Color)
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     *
+     * @return res (String)
+     *
+     * Chaque couleur correspond à un type de Sapiens
+     * afin de pouvoir les reconnaître sur la simulation
+     *
+     */
     public String toString(){
         String res="";
         if(color==Color.YELLOW){
             res="Susceptible";
         }
-        else if(color==Color.GREEN){
+        else if(color==Color.BLUE){
             res="Recovered";
         }
         if(color==Color.BLACK){
